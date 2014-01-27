95b64687404018684b3bd7382bfadde36528142b
Matej Koubik <matej@koubik.name>
2014-01-27 20:40:19 +0100

    typos

--- src/LatteBundle/Bridge/Nette/Latte/Macros/AssetMacros.php
+++ src/LatteBundle/Bridge/Nette/Latte/Macros/AssetMacros.php
@@ -6,15 +6,15 @@ use Nette\Latte;
 
 class AssetMacros extends Latte\Macros\MacroSet
 {
-	public static function install(Compiler $compiler)
+	public static function install(Latte\Compiler $compiler)
 	{
 		$me = new static($compiler);
 
 		$me->addMacro('asset', array($me, 'macroAsset'));
 	}
 
-	public function macroAsset(MacroNode $node, PhpWriter $writer)
+	public function macroAsset(Latte\MacroNode $node, Latte\PhpWriter $writer)
 	{
-		return $writer->write('echo %escape($assets->getAssetUrl(%node.args))');
+		return $writer->write('echo %escape($assets->getUrl(%node.args))');
 	}
 }
