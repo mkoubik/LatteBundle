86c18f342d1fd3fc0b3962e1adc7ab7575448249
Matej Koubik <matej@koubik.name>
2014-04-13 18:54:08 +0200

    form macros: templates for choice widgets (select box)

--- src/LatteBundle/Bridge/Symfony/Templating/Helper/FormHelper.php
+++ src/LatteBundle/Bridge/Symfony/Templating/Helper/FormHelper.php
@@ -3,11 +3,24 @@
 namespace LatteBundle\Bridge\Symfony\Templating\Helper;
 
 use Symfony\Bundle\FrameworkBundle\Templating\Helper\FormHelper as BaseHelper;
+use Symfony\Component\Form\Extension\Core\View\ChoiceView;
 
 class FormHelper extends BaseHelper
 {
     public function getName()
     {
         return 'latteForms';
+    }
+
+    /**
+     * From Symfony\Bridge\Twig\Extension\FormExtension
+     */
+    public function isSelectedChoice(ChoiceView $choice, $selectedValue)
+    {
+        if (is_array($selectedValue)) {
+            return false !== array_search($choice->value, $selectedValue, true);
+        }
+
+        return $choice->value === $selectedValue;
     }
 }
