56ea92f4badceaddd904276f3bf4b00d8bdd3b05
Matej Koubik <matej@koubik.name>
2014-05-29 21:24:33 +0200

    allowed overriding vars by {form_widget $input, full_name => abc}

--- src/LatteBundle/Bridge/Nette/Latte/Macros/FormMacros.php
+++ src/LatteBundle/Bridge/Nette/Latte/Macros/FormMacros.php
@@ -19,21 +19,47 @@ class FormMacros extends MacroSet
         $me->addMacro('form_end', array($me, 'macroForm'));
         $me->addMacro('form_rest', array($me, 'macroForm'));
         $me->addMacro('form_row', array($me, 'macroForm'));
-        $me->addMacro('form_label', array($me, 'macroForm'));
+        $me->addMacro('form_label', array($me, 'macroFormLabel'));
         $me->addMacro('form_widget', array($me, 'macroForm'));
-        $me->addMacro('form_errors', array($me, 'macroForm'));
+        $me->addMacro('form_errors', array($me, 'macroFormErrors'));
 
         $me->addMacro('form_theme', '$_latteForms->setTheme(%node.word, %node.array?)');
     }
 
     public function macroForm(MacroNode $node, PhpWriter $writer)
     {
-        $name = $node->tokenizer->fetchWord();
-        if ($name === FALSE) {
+        $form = $node->tokenizer->fetchWord();
+        if ($form === FALSE) {
             throw new CompileException("Missing form name in {{$node->name}}.");
         }
-        $node->tokenizer->reset();
         $block = Strings::startsWith($node->name, 'form_') ? substr($node->name, 5) : $node->name;
-        return $writer->write('echo $_latteForms->' . $block . '(' . $name . ')');
+        $form = $writer->formatWord($form);
+        $vars = $writer->formatArray();
+        return $writer->write('echo $_latteForms->' . $block . '(' . $form . ', ' . $vars . ')');
+    }
+
+    public function macroFormLabel(MacroNode $node, PhpWriter $writer)
+    {
+        $form = $node->tokenizer->fetchWord();
+        if ($form === FALSE) {
+            throw new CompileException("Missing form name in {{$node->name}}.");
+        }
+        $block = Strings::startsWith($node->name, 'form_') ? substr($node->name, 5) : $node->name;
+        $form = $writer->formatWord($form);
+        $label = $node->tokenizer->fetchWord();
+        $label = $label ? $writer->formatWord($label) : 'null';
+        $vars = $writer->formatArray();
+        return $writer->write('echo $_latteForms->' . $block . '(' . $form . ', ' . $label . ', ' . $vars . ')');
+    }
+
+    public function macroFormErrors(MacroNode $node, PhpWriter $writer)
+    {
+        $form = $node->tokenizer->fetchWord();
+        if ($form === FALSE) {
+            throw new CompileException("Missing form name in {{$node->name}}.");
+        }
+        $block = Strings::startsWith($node->name, 'form_') ? substr($node->name, 5) : $node->name;
+        $form = $writer->formatWord($form);
+        return $writer->write('echo $_latteForms->' . $block . '(' . $form . ')');
     }
 }
